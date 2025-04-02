1a4de57b6fab40feb211c79042d6f90f6f1fcc88
youwei.cao <gofar@outlook.com>
2025-04-02 13:03:13 +0800

    file transfer

---
+++ homework3/src/closeloop/src/closed_loop.cpp
@@ -0,0 +1,21 @@
+#include "rclcpp/rclcpp.hpp"
+#include <iostream>
+#include "geometry_msgs/msg/twist.hpp"
+#include <cmath>
+
+class Turtlebot : public rclcpp::Node
+{
+private:
+    /* data */
+public:
+    Turtlebot(/* args */);
+    ~Turtlebot();
+};
+
+Turtlebot::Turtlebot(/* args */)
+{
+}
+
+Turtlebot::~Turtlebot()
+{
+}
